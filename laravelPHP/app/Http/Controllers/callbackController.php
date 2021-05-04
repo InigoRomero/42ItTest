154500b98bdd1eb7a94079abe18a1793a8966e17
InigoRomero <igoromero@gmail.com>
2021-05-04 16:44:26 +0200

    laravel conected to api

--- laravelPHP/app/Http/Controllers/callbackController.php
+++ laravelPHP/app/Http/Controllers/callbackController.php
@@ -18,42 +18,28 @@ class callbackController extends Controller
     public function callback(Request $request)
     {
         $code = $request->code;
-
-
-      /*  $provider = new GenericProvider([
-            'clientId'                => env("CLIENT_ID", "somedefaultvalue") ,   // The client ID assigned to you by the provider
-            'clientSecret'            => env("CLIENT_SECRET", "somedefaultvalue") ,    // The client password assigned to you by the provider
-            'redirectUri'             => env("REDIRECT_URI", "somedefaultvalue") ,
-            'urlAuthorize'            => env("AUTHORIZATION_URI", "somedefaultvalue") ,
-            'urlAccessToken'          => env("ACCESS_TOKEN_URI", "somedefaultvalue") ,
-            'urlResourceOwnerDetails' => 'https://api.intra.42.fr/v2/me'
-        ]);
-        $accessToken = $provider->getAccessToken('authorization_code', [
-            'code' => $code
-        ]);*/
         $client = new Client([
             'base_uri' => 'https://api.intra.42.fr',
             'timeout'  => 50000000.0,
         ]);
+        echo  env("REDIRECT_URI", "");
         $response = $client->request('POST', env("ACCESS_TOKEN_URI", ""), [
-
             'headers'  => [
-                'Content-Type' => 'multipart/form-data',
-                'Authorization:' => "Bearer " . $code
+                'Content-Type' => 'application/json'
             ],
-            'form_params' => [
-                
+            'body' => json_encode([
                 'grant_type' => 'authorization_code',
                 'client_id' => env("CLIENT_ID", "") ,
+                "scope" => "public" ,
                 'client_secret' => env("CLIENT_SECRET", ""), 
                 'code' => $code,
                 'redirect_uri' => env("REDIRECT_URI", "")
-            ]
+            ])
         ]);
         $token = $response->getBody();
         $token = json_decode($token, true);
         echo "Bearer " .$token["access_token"];
-        $response = $client->request('GET', "https://api.intra.42.fr/v2/users", [
+        $response = $client->request('GET', "https://api.intra.42.fr/v2/me", [
             'headers'  => [
                 'Authorization' => "Bearer " . $token["access_token"]
             ]
@@ -63,25 +49,3 @@ class callbackController extends Controller
     }
 
 }
-
-/*            'multipart' => [
-                [
-                    'name'     => 'grant_type',
-                    'contents' => 'authorization_code'
-                ],
-                [
-                    'name'     => 'client_id',
-                    'contents' => env("CLIENT_ID", "")
-                ],                
-                [
-                    'name'     => 'client_secret',
-                    'contents' => env("CLIENT_SECRET", "")
-                ],
-                [
-                    'name'     => 'code',
-                    'contents' => $code
-                ],
-                [
-                    'name'     => 'redirect_uri',
-                    'contents' => env("REDIRECT_URI", "")
-                ],*/
